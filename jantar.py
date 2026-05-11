5ad259b36c3f6ea40669199a839676dc030282ac
Chxrry_28 <raissa.bernardoferreira28@gmail.com>
2026-05-11 10:41:55 -0300

    11/05/26

--- jantar.py
+++ jantar.py
@@ -8,11 +8,11 @@ pratos = [0, 0, 0, 0, 0]  # 0 = Não comeu, 1 = Já comeu
 class Filosofo(Thread):
     execute = True  # variável para realizar a execução
 
-    def __init__(self, nome, hashi_esquerda, hashi_direita):  # Construtor da classe Filosofo
+    def __init__(self, nome, garfo_esquerda, garfo_direita):  # Construtor da classe Filosofo
         Thread.__init__(self)
         self.nome = nome
-        self.hashi_esquerda = hashi_esquerda
-        self.hashi_direita = hashi_direita
+        self.garfo_esquerda = garfo_esquerda
+        self.garfo_direita = garfo_direita
 
     def run(self):
         """ Sobrescrita de Thread, a função run definirá o que irá acontecer após chamar o método start() na
@@ -24,18 +24,18 @@ class Filosofo(Thread):
 
     def comer(self):
         """
-        Pega o hashi 1 e tenta pegar o hashi 2. Se o hashi 2 estiver livre,
-        o ele janta e solta os dois hashis em seguida,senão ele desiste de
+        Pega o garfo 1 e tenta pegar o garfo 2. Se o garfo 2 estiver livre,
+        o ele janta e solta os dois garfos em seguida,senão ele desiste de
         comer e continua pensando.
         """
-        hashi1, hashi2 = self.hashi_esquerda, self.hashi_direita
+        garfo1, garfo2 = self.garfo_esquerda, self.garfo_direita
 
         while self.execute:  # enquanto tiver executando
-            hashi1.acquire(True)  # tenta pegar o primeiro hashi
-            locked = hashi2.acquire(False)  # verifica se o segundo hashi está livre
+            garfo1.acquire(True)  # tenta pegar o primeiro garfo
+            locked = garfo2.acquire(False)  # verifica se o segundo garfo está livre
             if locked:
                 break
-            hashi1.release()  # libera o hashi1
+            garfo1.release()  # libera o garfo1
         else:
             return  # volta a pensar
 
@@ -44,13 +44,13 @@ class Filosofo(Thread):
         print(f"\n {self.nome} parou de comer")
         pratos[nomes.index(self.nome)] += 1  # contabiliza o número de vezes que cada filosofo comeu
         print(pratos)
-        hashi1.release()  # libera o hashi1
-        hashi2.release()  # libera o hashi2
+        garfo1.release()  # libera o garfo1
+        garfo2.release()  # libera o garfo2
 
 
 nomes = ['Aristóteles', 'Platão', 'Sócrates', 'Pitágoras', 'Demócrito']  # Nomes dos filósofos
-hashis = [Lock() for _ in range(5)]
-mesa = [Filosofo(nomes[i], hashis[i % 5], hashis[(i + 1) % 5]) for i in range(5)]
+garfos = [Lock() for _ in range(5)]
+mesa = [Filosofo(nomes[i], garfos[i % 5], garfos[(i + 1) % 5]) for i in range(5)]
 for _ in range(50):
     Filosofo.execute = True  # Inicia a execução
     for filosofo in mesa:
